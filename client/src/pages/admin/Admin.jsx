83ed3d970c608a87cbc86fde600ea290f6c5bd7d
vikrantkumar <chaudharyvikrant2000@gmail.com>
2023-10-01 17:45:40 +0530

    admin panel development

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../../components/admin/Sidebar";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <main className="flex justify-start items-start">
+      <Sidebar />
+      <div className="w-full">
+        <Outlet />
+      </div>
+    </main>
+  );
 };
 
 export default Admin;
